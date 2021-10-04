521035c82053b631228b93553cccb323021e8d32
manishkalpande <manishkalpande23@gmail.com>
2021-10-04 16:21:12 +0530

    first commit

---
+++ src/main/java/com/shetbajar/shetBajar/ShetBajarApplication.java
@@ -0,0 +1,13 @@
+package com.shetbajar.shetBajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShetBajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShetBajarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shetbajar/shetBajar/ShetBajarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shetbajar.shetBajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShetBajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
